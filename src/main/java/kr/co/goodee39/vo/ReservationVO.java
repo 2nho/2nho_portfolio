4eb76f0f88e6cba8a51c16105dd01a238a1611e0
2nho <kim2nho@gmail.com>
2022-03-08 22:52:37 +0900

    commit

--- src/main/java/kr/co/goodee39/vo/ReservationVO.java
+++ src/main/java/kr/co/goodee39/vo/ReservationVO.java
@@ -11,17 +11,15 @@ public class ReservationVO {
 	private int start;
 	private int count;
 	
-	
-	
 	public ReservationVO() {
 		this.start = 0;
 		this.count = 10;
 	}
-
 	
 	public int getRnum() {
 		return rnum;
 	}
+
 
 
 	public void setRnum(int rnum) {
@@ -29,9 +27,23 @@ public class ReservationVO {
 	}
 
 
+
+	public int getMnum() {
+		return mnum;
+	}
+
+
+
+	public void setMnum(int mnum) {
+		this.mnum = mnum;
+	}
+
+
+
 	public int getBdiv() {
 		return bdiv;
 	}
+
 
 
 	public void setBdiv(int bdiv) {
@@ -39,144 +51,29 @@ public class ReservationVO {
 	}
 
 
-	public int getMnum() {
-		return mnum;
-	}
-
-
-	public void setMnum(int mnum) {
-		this.mnum = mnum;
-	}
-
-	public String getReserName() {
-		return reserName;
-	}
-
-
-	public void setReserName(String reserName) {
-		this.reserName = reserName;
-	}
-
 
 	public String getContent() {
 		return content;
 	}
 
+
+
 	public void setContent(String content) {
 		this.content = content;
 	}
 
-	public String getReserDate() {
-		return reserDate;
-	}
 
-	public void setReserDate(String reserDate) {
-		this.reserDate = reserDate;
-	}
-
-	public String getKindCd() {
-		return kindCd;
-	}
-
-	public void setKindCd(String kindCd) {
-		this.kindCd = kindCd;
-	}
-
-	public String getSexCd() {
-		return sexCd;
-	}
-
-	public void setSexCd(String sexCd) {
-		this.sexCd = sexCd;
-	}
-
-	public String getAge() {
-		return age;
-	}
-
-	public void setAge(String age) {
-		this.age = age;
-	}
-
-	public String getSpecialMark() {
-		return specialMark;
-	}
-
-	public void setSpecialMark(String specialMark) {
-		this.specialMark = specialMark;
-	}
-
-	public String getProcessState() {
-		return processState;
-	}
-
-	public void setProcessState(String processState) {
-		this.processState = processState;
-	}
-
-	public String getNoticeSdt() {
-		return noticeSdt;
-	}
-
-	public void setNoticeSdt(String noticeSdt) {
-		this.noticeSdt = noticeSdt;
-	}
-
-	public String getHappenPlace() {
-		return happenPlace;
-	}
-
-	public void setHappenPlace(String happenPlace) {
-		this.happenPlace = happenPlace;
-	}
-
-	public String getDogImg() {
-		return dogImg;
-	}
-
-	public void setDogImg(String dogImg) {
-		this.dogImg = dogImg;
-	}
-
-	public String getCareNm() {
-		return careNm;
-	}
-
-	public void setCareNm(String careNm) {
-		this.careNm = careNm;
-	}
-
-	public String getCareAddr() {
-		return careAddr;
-	}
-
-	public void setCareAddr(String careAddr) {
-		this.careAddr = careAddr;
-	}
-
-	public String getCareTel() {
-		return careTel;
-	}
-
-	public void setCareTel(String careTel) {
-		this.careTel = careTel;
-	}
-
-	public String getChargeNm() {
-		return chargeNm;
-	}
-
-	public void setChargeNm(String chargeNm) {
-		this.chargeNm = chargeNm;
-	}
 
 	public String getDel() {
 		return del;
 	}
 
+
+
 	public void setDel(String del) {
 		this.del = del;
 	}
+
 
 
 	public int getStart() {
@@ -184,9 +81,11 @@ public class ReservationVO {
 	}
 
 
+
 	public void setStart(int start) {
 		this.start = start;
 	}
+
 
 
 	public int getCount() {
@@ -194,9 +93,11 @@ public class ReservationVO {
 	}
 
 
+
 	public void setCount(int count) {
 		this.count = count;
 	}
+
 	
 	
 }
