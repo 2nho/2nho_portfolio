475974371639c99cdb3b416fa70d4adc488b85cf
2nho <kim2nho@gmail.com>
2022-06-04 22:31:59 +0900

    컨트롤러 이름 변경

---
+++ src/main/java/kr/co/goodee39/controller/BasicController.java
@@ -0,0 +1,30 @@
+package kr.co.goodee39.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.co.goodee39.vo.MemberVO;
+
+@Controller
+public class BasicController {
+	@GetMapping("/login")
+		public String login() {
+			return "login";
+	}
+	
+	@GetMapping("/signUp")
+	public String signUp(MemberVO vo) {
+		 
+		return "signup";
+	}
+	@GetMapping("/jeju")
+	public String jeju(MemberVO vo) {
+		 
+		return "jeju";
+	}
+	@GetMapping("/germany")
+	public String germany(MemberVO vo) {
+		 
+		return "germany";
+	}
+}
